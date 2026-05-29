5ceb82863ed98e0fc37684488f9526f2c0dcd6f6
Todd Zimmerman <zimmermant@uwstout.edu>
2026-05-29 14:30:45 -0500

    Fixed issue on my computer with saving.  Added in Garage for S3 and saving images

--- config/aws.js
+++ config/aws.js
@@ -1,10 +1,24 @@
 var AWS      = require('aws-sdk')
     , config = require('config');
 
-AWS.config.update({
+var awsConfig = {
   accessKeyId       : config.aws.keyId
   , secretAccessKey : config.aws.key
   , region          : config.aws.region
-});
+};
+
+if (config.aws.endpoint) {
+  awsConfig.endpoint = config.aws.endpoint;
+}
+
+if (config.aws.s3ForcePathStyle !== undefined) {
+  awsConfig.s3ForcePathStyle = config.aws.s3ForcePathStyle;
+}
+
+if (config.aws.signatureVersion) {
+  awsConfig.signatureVersion = config.aws.signatureVersion;
+}
+
+AWS.config.update(awsConfig);
 
 module.exports = AWS;
